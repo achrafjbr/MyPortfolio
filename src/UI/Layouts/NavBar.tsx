046040c71726a5ab2f5024c20f89db665ebf9b2e
achrafjbr <developer@gmail.com>
2026-06-08 16:12:47 +0200

    Start building Portfolio project

---
+++ src/UI/Layouts/NavBar.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import { JSX } from "react";
+
+function NavBar(): JSX.Element {
+  return (
+    <div className="bg-gray-200/5 shadow flex py-6 justify-around font-medium text-gray-500 ">
+      <div className="flex flex-1 justify-center items-center gap-x-4">
+        <Link className="hover:text-black" href={"/"}>
+          Accueil
+        </Link>
+        <Link className="hover:text-black" href={"/about"}>
+          À propos
+        </Link>
+        <Link className="hover:text-black" href={"/portfolio"}>
+          Portfolio
+        </Link>
+        <Link className="hover:text-black" href={"/skills"}>
+          Compétences
+        </Link>
+        <Link className="hover:text-black" href={"/contact"}>
+          Contact
+        </Link>
+      </div>
+
+      <div className="flex items-center justify-end gap-x-1.5 mr-2">
+        <h1>dowlnlaodcv</h1>
+        <h1>theme</h1>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <section className="flex flex-col pt-[10%] text-center items-center gap-y-1">
+      <h1 className="text-5xl md:text-6xl mb-2  font-semibold">
+        Bonjour, je suis Achraf El jabbar
+      </h1>
+      <p className="text-2xl md:text-3xl text-muted-foreground mb-3 font-light text-gray-500 ">
+        Développeur Fullstack & Flutter
+      </p>
+      <p className="text-lg text-muted-foreground max-w-2xl mx-auto mb-8  text-center text-gray-500">
+        Passionné par la création d applications web modernes et performantes.
+        Je transforme des idées en solutions numériques élégantes.
+      </p>
+    </section>
+  );
+}
